01dce977f9c89e375a266d6d7db3f63dde3d8a1d
jayeshdubey <jayeshdubey147@gmail.com>
2016-10-01 10:08:24 +0530

    commit

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+print("program to identify if the number is even or odd ")
+
+number = int(input("enter  a number"))
+
+if number % 2 == 0:
+    print("the number is even")
+else:
+    print('the number is odd')
